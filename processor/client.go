9aea2d7de939d86ca74b551ace8a078295f7d736
sunist-c <sunist@mail.sunist.cn>
2023-10-10 19:28:45 +0800

    update: 适配新的日志记录器

--- processor/client.go
+++ processor/client.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"github.com/tencent-connect/botgo/openapi"
+	"studio.sunist.work/sunist-c/ceobebot-qqchanel/infrastructure/log"
 	"studio.sunist.work/sunist-c/ceobebot-qqchanel/processor/message"
 )
 
@@ -18,11 +19,19 @@ func (c *client) ReplyMessage(ctx Context, reply message.Message) {
 	payload := ctx.GetPayload()
 	msg := reply.Build()
 	msg.MsgID = payload.Message.ID
-	_, _ = c.api.PostMessage(ctx.GetContext(), payload.Message.ChannelID, msg)
+	result, err := c.api.PostMessage(ctx.GetContext(), payload.Message.ChannelID, msg)
+	if err != nil {
+		result, _ = c.api.PostMessage(ctx.GetContext(), payload.Message.ChannelID, message.NewTextMessage().Text("回复被夹掉了，换个话题吧").Build())
+	}
+	logger.Info(log.NewFieldsWithMessage("message sent").With("content", result.Content))
 }
 
 func (c *client) SendMessage(ctx Context, content message.Message) {
-	_, _ = c.api.PostMessage(ctx.GetContext(), ctx.GetPayload().Message.ChannelID, content.Build())
+	result, err := c.api.PostMessage(ctx.GetContext(), ctx.GetPayload().Message.ChannelID, content.Build())
+	if err != nil {
+		result, _ = c.api.PostMessage(ctx.GetContext(), ctx.GetPayload().Message.ChannelID, message.NewTextMessage().Text("回复被夹掉了，换个话题吧").Build())
+	}
+	logger.Info(log.NewFieldsWithMessage("message sent").With("content", result.Content))
 }
 
 func NewClient(api openapi.OpenAPI) Client {
--- processor/event.go
+++ processor/event.go
@@ -7,11 +7,13 @@ import (
 	"github.com/tencent-connect/botgo/event"
 	"github.com/tencent-connect/botgo/openapi"
 	"strings"
+	"studio.sunist.work/sunist-c/ceobebot-qqchanel/infrastructure/log"
 )
 
 func AtMessageEventHandler(api openapi.OpenAPI) event.ATMessageEventHandler {
 	return func(event *dto.WSPayload, data *dto.WSATMessageData) error {
 		input := strings.ToLower(message.ETLInput(data.Content))
+		logger.Info(log.NewFieldsWithMessage("message received").With("content", input))
 		cmd := message.ParseCommand(input)
 		mustHandlers, optionalHandlers := DefaultMatcher().MatchHandlers(cmd.Cmd, cmd.Content)
 
